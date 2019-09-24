100aad83417e3aa770bdda8b7ab2c05c484f3c05
yennanliu <f339339@gmail.com>
2019-09-25 00:10:27 +0800

    add py script process api reponse and save to scsv

---
+++ commit_2_csv.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import pandas as pd 
+import urllib 
+import json 
+
+def main(url):
+    #url = 'https://api.github.com/repos/apache/airflow/commits'
+    opener=urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    page = opener.open(url)
+    soup = BeautifulSoup(page,"html.parser")
+    # BeautifulSoup response -> text -> json 
+    data_dict = json.loads(soup.getText())
+    print (data_dict)
+    df_cols = list(data_dict[0].keys())
+    collected = [ []  for i in range(len(df_cols))]
+    for i in range(len(data_dict)):
+        for index, col in enumerate(df_cols):
+            collected[index].append(data_dict[i][col])
+    # init dataframe 
+    df = pd.DataFrame()
+    for index, col in enumerate(df_cols):
+        df[col] = collected[index]
+    print (df)
+    df.to_csv('commit_sample.csv')
+    return df 
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/apache/airflow/commits'
+    main(url)
