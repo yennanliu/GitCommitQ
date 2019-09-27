56c007607332116ce14e6568d86498652e51c288
yennanliu <f339339@gmail.com>
2019-09-27 16:28:07 +0800

    add create db,user, password, grand access commands

--- src/start_postgre.sh
+++ src/start_postgre.sh
@@ -6,3 +6,12 @@ echo '>>>> stop postgresql'
 brew services stop postgresql
 echo '>>>> start postgresql'
 brew services start postgresql
+echo '>>>> creat DB'
+sudo -u yennanliu createdb gitcommit  
+echo '>>>> creat user'
+sudo -u yennanliu createuser postgre_user
+echo '>>>> creat user password'
+# $ sudo -u yennanliu psql
+# psql=# alter user postgre_user with encrypted password '0000';
+echo '>>>> grant user db access'
+# psql=# grant all privileges on database gitcommit  to postgre_user ;
