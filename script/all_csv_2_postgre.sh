84d249cb1b6bcceda5a296e72519f7b58cc5dd11
yennanliu <f339339@gmail.com>
2019-09-27 16:49:27 +0800

    add script dump csv to postgre

---
+++ script/all_csv_2_postgre.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#################################################################
+# SCRIPTS DUMP ALL CSV TO MYSQL    
+#################################################################
+python script/csv_2_postgre.py data/commit_sample.csv  commit_raw
