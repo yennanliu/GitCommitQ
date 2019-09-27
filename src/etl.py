60560488714061569cf840ab6756bd5dd9834943
yennanliu <f339339@gmail.com>
2019-09-28 01:09:54 +0800

    fix code layout

--- src/etl.py
+++ src/etl.py
@@ -7,11 +7,11 @@ from process_commit import *
 from dump_to_postgre import * 
 
 def main(repo_url, table_name):
-	df = Commit2df(repo_url)
-	output_df = extract_inform(df)
-	postgre_config = parse_config('config/postgre.config')
-	connection = get_conn(postgre_config)
-	insert_to_table(output_df,table_name,connection)
+    df = Commit2df(repo_url)
+    output_df = extract_inform(df)
+    postgre_config = parse_config('config/postgre.config')
+    connection = get_conn(postgre_config)
+    insert_to_table(output_df,table_name,connection)
 
 if __name__ == '__main__':
     #repo_url = 'https://api.github.com/repos/tensorflow/tensorflow/commits'
