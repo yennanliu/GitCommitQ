12915684bd5f6ce3cfcef0c5c83d1e617c2beb2c
yennanliu <f339339@gmail.com>
2019-09-30 18:09:35 +0800

    add weekday-hourgroup heatmap sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,4 +1,5 @@
--- List the top 3 authors in the given time period?
+-- List the top 3 authors in the given time period
+
 SELECT user_id
 FROM
   (SELECT user_id,
@@ -11,6 +12,7 @@ FROM
    LIMIT 3) sub
 
 -- Find the author with the longest contribution window within the time period?
+
 SELECT distinct(user_id)
 FROM git_commit
 WHERE user_id IN
@@ -28,3 +30,49 @@ WHERE user_id IN
         LIMIT 1) sub_)
 
 -- Produce a heatmap of commits 
+WITH commit_weekday_hour AS
+  (SELECT commit_id,
+          date_part('hour', commit_timestamp) AS HOUR,
+          to_char(commit_timestamp, 'Day') AS weekday
+   FROM git_commit),
+     commit_weekday_hourgroup AS
+  (SELECT *,
+          CASE
+              WHEN HOUR BETWEEN 0 AND 3 THEN '12am-3am'
+              WHEN HOUR BETWEEN 3 AND 6 THEN '3am-6am'
+              WHEN HOUR BETWEEN 6 AND 9 THEN '6am-9am'
+              WHEN HOUR BETWEEN 9 AND 12 THEN '9am-12pm'
+              WHEN HOUR BETWEEN 12 AND 15 THEN '12pm-3pm'
+              WHEN HOUR BETWEEN 15 AND 18 THEN '3pm-6pm'
+              WHEN HOUR BETWEEN 18 AND 21 THEN '6pm-9pm'
+              WHEN HOUR BETWEEN 21 AND 24 THEN '9pm-12am'
+              ELSE 'none'
+          END AS hour_group
+   FROM commit_weekday_hour)
+SELECT hour_group,
+       sum(CASE
+               WHEN weekday = 'Sunday' THEN 1
+               ELSE 0
+           END) AS Sun,
+       sum(CASE
+               WHEN weekday = 'Monday' THEN 1
+               ELSE 0
+           END) AS Mon,
+       sum(CASE
+               WHEN weekday = 'Tuesday' THEN 1
+               ELSE 0
+           END) AS Tue,
+       sum(CASE
+               WHEN weekday = 'Wednesday' THEN 1
+               ELSE 0
+           END) AS Wed,
+       sum(CASE
+               WHEN weekday = 'Thursday' THEN 1
+               ELSE 0
+           END) AS Thur,
+       sum(CASE
+               WHEN weekday = 'Friday' THEN 1
+               ELSE 0
+           END) AS Fri
+FROM commit_weekday_hourgroup
+GROUP BY 1;
