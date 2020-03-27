77eb4702eae4b2d2512387807ee0d72f3004bb36
yennanliu <f339339@gmail.com>
2020-03-27 12:03:54 +0800

    split db, data process test to different test, and rename them (test_DBOp.py, test_DataProcess.py)

---
+++ tests/test_DBOp.py
@@ -0,0 +1,81 @@
+import datetime
+import pandas as pd
+import psycopg2
+import sys
+sys.path.append(".")
+from src.dump_to_postgre import DumpToPostgre as DumpToPostgre_
+from script.utility import parse_config as parse_config_
+
+ 
+postgre_config = {
+  "url":             "jdbc:postgres://localhost/gitcommit",
+  "host":            "127.0.0.1",
+  "dbname":          "gitcommit",
+  "dbtable_batch":   "fulltable",
+  "dbtable_stream":  "rddbatch",
+  "mode_batch":      "overwrite",
+  "mode_stream":     "append",
+  "user":            "postgre_user",
+  "password":        "0000",
+  "driver":          "com.postgres.jdbc.Driver",
+  "numPartitions":   18,
+  "partitionColumn": "time_slot",
+  "lowerBound":      0,
+  "upperBound":      144,
+  "stringtype":      "unspecified",
+  "topntosave":      10 }
+  
+data = {'id' : ['jack','mary'], 'age' : [10, 90]}
+df = pd.DataFrame(data)
+
+
+def test_get_conn():
+    dump_to_postgre_ = DumpToPostgre_()
+    db_conn= dump_to_postgre_.get_conn(postgre_config)
+    with db_conn.cursor() as cursor:
+        cursor.execute("SELECT 1")
+        result=cursor.fetchall()
+    assert type(db_conn) == psycopg2.extensions.connection and result[0][0] == 1
+
+def test_insert_to_table():
+    dump_to_postgre_ = DumpToPostgre_()
+    db_conn= dump_to_postgre_.get_conn(postgre_config)
+    # create table 
+    schema = "(id VARCHAR (10), age integer)"
+    dump_to_postgre_.create_table('test_table', schema, postgre_config)
+    # insert df to table
+    dump_to_postgre_.insert_to_table(df, 'test_table', postgre_config)
+    with db_conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM test_table")
+        result=cursor.fetchall()
+    # TODO : drop table 
+    assert result == [('jack', 10), ('mary', 90)]
+
+def test_insert_all_to_table():
+    # load to-insert df 
+    df = pd.read_csv('data/movie_ratings.csv')
+    dump_to_postgre_ = DumpToPostgre_()
+    db_conn= dump_to_postgre_.get_conn(postgre_config)
+    schema = "(index integer, userId integer, movieId integer, rating integer, timestamp integer)"
+    # create table 
+    dump_to_postgre_.create_table('movie_table', schema, postgre_config)
+    # insert all to table 
+    dump_to_postgre_.insert_all_to_table(df, 'movie_table', postgre_config)
+    with db_conn.cursor() as cursor:
+        cursor.execute("SELECT count(*) FROM movie_table")
+        result=cursor.fetchall()
+    # TODO : drop table 
+    #dump_to_postgre_.drop_table('movie_table', postgre_config)
+    assert result[0][0] == 100004
+
+
+def test_parse_config():
+    config = parse_config_("config/postgre.config")
+    assert config['url'] == "jdbc:postgres://localhost/gitcommit"
+    assert config['host'] == "127.0.0.1"
+    assert config['dbname'] == "gitcommit"
+    assert config['user'] == "postgre_user"
+    assert config['driver'] == "com.postgres.jdbc.Driver"
+
+if __name__ == '__main__':
+    pytest.main([__file__])
--- tests/test_DataProcess.py
+++ tests/test_DataProcess.py
@@ -1,10 +1,8 @@
 import datetime
 import pandas as pd
-import psycopg2
 import sys
 sys.path.append(".")
 from src.get_commit import Commit2df as Commit2df_
-from src.dump_to_postgre import DumpToPostgre as DumpToPostgre_
 from src.process_commit import (get_commit_timestamp as get_commit_timestamp_,
                                 generate_id as generate_id_,
                                 get_user_id as get_user_id_,
@@ -12,24 +10,6 @@ from src.process_commit import (get_commit_timestamp as get_commit_timestamp_,
                                 extract_inform as extract_inform_)
 from script.utility import parse_config as parse_config_
 
- 
-postgre_config = {
-  "url":             "jdbc:postgres://localhost/gitcommit",
-  "host":            "127.0.0.1",
-  "dbname":          "gitcommit",
-  "dbtable_batch":   "fulltable",
-  "dbtable_stream":  "rddbatch",
-  "mode_batch":      "overwrite",
-  "mode_stream":     "append",
-  "user":            "postgre_user",
-  "password":        "0000",
-  "driver":          "com.postgres.jdbc.Driver",
-  "numPartitions":   18,
-  "partitionColumn": "time_slot",
-  "lowerBound":      0,
-  "upperBound":      144,
-  "stringtype":      "unspecified",
-  "topntosave":      10 }
   
 data = {'id' : ['jack','mary'], 'age' : [10, 90]}
 df = pd.DataFrame(data)
@@ -60,58 +40,11 @@ def test_extract_inform():
     result_df = extract_inform_(df)
     assert len(result_df) == 0
 
-def test_get_conn():
-    dump_to_postgre_ = DumpToPostgre_()
-    db_conn= dump_to_postgre_.get_conn(postgre_config)
-    with db_conn.cursor() as cursor:
-        cursor.execute("SELECT 1")
-        result=cursor.fetchall()
-    assert type(db_conn) == psycopg2.extensions.connection and result[0][0] == 1
-
-def test_insert_to_table():
-    dump_to_postgre_ = DumpToPostgre_()
-    db_conn= dump_to_postgre_.get_conn(postgre_config)
-    # create table 
-    schema = "(id VARCHAR (10), age integer)"
-    dump_to_postgre_.create_table('test_table', schema, postgre_config)
-    # insert df to table
-    dump_to_postgre_.insert_to_table(df, 'test_table', postgre_config)
-    with db_conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM test_table")
-        result=cursor.fetchall()
-    # TODO : drop table 
-    assert result == [('jack', 10), ('mary', 90)]
-
-def test_insert_all_to_table():
-    # load to-insert df 
-    df = pd.read_csv('data/movie_ratings.csv')
-    dump_to_postgre_ = DumpToPostgre_()
-    db_conn= dump_to_postgre_.get_conn(postgre_config)
-    schema = "(index integer, userId integer, movieId integer, rating integer, timestamp integer)"
-    # create table 
-    dump_to_postgre_.create_table('movie_table', schema, postgre_config)
-    # insert all to table 
-    dump_to_postgre_.insert_all_to_table(df, 'movie_table', postgre_config)
-    with db_conn.cursor() as cursor:
-        cursor.execute("SELECT count(*) FROM movie_table")
-        result=cursor.fetchall()
-    # TODO : drop table 
-    #dump_to_postgre_.drop_table('movie_table', postgre_config)
-    assert result[0][0] == 100004
-
 def test_Commit2df():
     expected_cols = ['comments_url','node_id','commit','parents','url','author','sha','html_url','committer']
     url = "https://api.github.com/repos/mlflow/mlflow/commits?since=2019-01-01T00:00:00Z&until=2019-01-01T23:59:59Z"
     df = Commit2df_(url)
     assert list(df.columns) == expected_cols
-
-def test_parse_config():
-    config = parse_config_("config/postgre.config")
-    assert config['url'] == "jdbc:postgres://localhost/gitcommit"
-    assert config['host'] == "127.0.0.1"
-    assert config['dbname'] == "gitcommit"
-    assert config['user'] == "postgre_user"
-    assert config['driver'] == "com.postgres.jdbc.Driver"
 
 if __name__ == '__main__':
     pytest.main([__file__])
